79c075b848134950cddee81b8a834836b6ca2eed
fernando <54969178+fherking@users.noreply.github.com>
2024-08-10 11:42:40 +0100

    Update http_h264.sh

--- profiles/http_h264.sh
+++ profiles/http_h264.sh
@@ -18,7 +18,7 @@ if [ -z "$CV" ] || [ -z "$VF" ]; then
   CV="h264"
 fi
 
-exec ffmpeg -hide_banner -loglevel warning \
+exec /run/current-system/sw/bin/ffmpeg -hide_banner -loglevel warning \
   $EXTRAPARAMS \
   -i "$INPUT" \
   -vf $VF \
